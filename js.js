20779750ed861b74d73f21ee55e7907564b97af2
Đặng Trường Giang <57037541+giangkma@users.noreply.github.com>
2022-03-27 21:41:02 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 let bai_tap_theo_lop = [
-     //Class Odnex
+     //Class Odne
      { class: 1, ex: '2 + 3 =', kq: 5 },
      { class: 1, ex: '1 + 3 =', kq: 4 },
      { class: 1, ex: '2 + 6 =', kq: 8 },
